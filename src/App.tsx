d58dffdd0a3a66d4795f1902b72b09c915451b53
Monjurul Alam <monjurulalam190629@gmail.com>
2025-02-12 00:31:45 +0600

    some changes are happened

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Person } from "./Person";
 
 
@@ -5,13 +6,26 @@ import { Person } from "./Person";
 
 function App() {
   const name:string = "Monjurul";
+  const [count, setCount] = useState(0);
 
+  const handleAdd = () => {
+    setCount(count + 1)
+  }
+  const handleMinus = () => {
+    setCount(count - 1)
+  }
   return (
+    
     <>
      <div>
       {name} is 25 years old.
      </div>
      <Person name={"Monjurul"} age = {"25"}/>
+     <Person name = {"Shourav"} age = {"24"} />
+     <div>
+        <button onClick={handleMinus}>-</button>{count}
+        <button onClick={handleAdd}>+</button>
+     </div>
     </>
   )
 }
